a803e628372b1172cb5e5ee5497cc756306f3597
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-12-07 07:50:53 +0300

    Initial Commit

---
+++ Bookworm APP/Bookworm_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm_APPApp.swift
+//  Bookworm APP
+//
+//  Created by Collins Muthomi on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
